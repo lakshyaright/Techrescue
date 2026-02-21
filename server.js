df8535b5f3338e96b6718a939dfac2b66e134faf
lakshyaright <lakshyaright@gmail.com>
2026-02-21 11:10:43 +0530

    Add bcrypt and jsonwebtoken dependencies

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const cors = require("cors");
 const { createClient } = require("@supabase/supabase-js");
