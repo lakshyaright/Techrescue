c00f9d9b0d7f57dc5f544a1b923b2893e4c07d0e
lakshyaright <lakshyaright@gmail.com>
2026-02-22 08:41:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { Server } = require("socket.io");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true });
+app.use(express.urlencoded({ extended: true }));
 /* =========================
    SUPABASE CONNECTION
 ========================= */
